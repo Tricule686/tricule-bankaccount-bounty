6f68ff23a179af09441eee9fd5de0e5b6f99c6d6
Tricule686 <111339417+Tricule686@users.noreply.github.com>
2023-09-12 13:05:28 +0100

    Add files via upload

---
+++ bounty/deploy/script.js
@@ -0,0 +1,61 @@
+// Bank Account class to manage accounts
+class BankAccount {
+    constructor(accountHolder, accountType, initialBalance) {
+        this.accountHolder = accountHolder;
+        this.accountType = accountType;
+        this.balance = initialBalance;
+    }
+
+    // Deposit funds into the account
+    deposit(amount) {
+        if (amount <= 0 || isNaN(amount)) {
+            throw new Error("Invalid deposit amount");
+        }
+        this.balance += amount;
+    }
+
+    // Withdraw funds from the account
+    withdraw(amount) {
+        if (amount <= 0 || isNaN(amount)) {
+            throw new Error("Invalid withdrawal amount");
+        }
+        if (amount > this.balance) {
+            throw new Error("Insufficient funds");
+        }
+        this.balance -= amount;
+    }
+}
+
+// Create a new bank account with initial values
+const account = new BankAccount("Tricule Stackup", "Savings", 1000);
+
+// Handle user actions when a button is clicked
+document.getElementById("performAction").addEventListener("click", () => {
+    const action = document.getElementById("action").value;
+    const amount = parseFloat(document.getElementById("amount").value);
+    const messageElement = document.getElementById("message");
+
+    try {
+        switch (action) {
+            case "deposit":
+                account.deposit(amount);
+                alert("Deposit successful!");
+                break;
+            case "withdraw":
+                account.withdraw(amount);
+                alert("Withdrawal successful!");
+                break;
+            case "checkBalance":
+                messageElement.textContent = `Current Balance: $${account.balance.toFixed(2)}`;
+                break;
+            default:
+                throw new Error("Invalid action");
+        }
+
+        document.getElementById("balance").textContent = account.balance.toFixed(2);
+    } catch (error) {
+        messageElement.textContent = error.message;
+    } finally {
+        document.getElementById("amount").value = "";
+    }
+});
